55da2d9bef41b2ae199630cd6b8b2f7c7b1f2007
NickDavis17 <063815@swcsd.us>
2022-02-09 14:26:49 -0500

    Almost done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,64 @@
 package com.company;
 
-import com.sun.security.jgss.GSSUtil;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
+    static ArrayList<Double> arrL = new ArrayList<>();
     public static void main(String[] args) {
-        Map<Integer, String> hashMap = new HashMap<>();
+        Map<String, Double> hashMap = new HashMap<>();
         userInput(hashMap);
+
 
 
 
     }
 
-    static void userInput(Map<Integer, String> a) {
+    static void userInput(Map<String, Double> a) {
 
-        String city = null;
-        while (city.equals("END")) {
+        String city = "null";
+        while (!city.equalsIgnoreCase("END")) {
             System.out.println("Please list a city:");
             city = input.next();
-            for (int j = 0; j < 5; j++) {
-                System.out.println("What is the temperature there?");
-                Integer temp = input.nextInt();
-                a.put(temp, city);
+            if (!city.equalsIgnoreCase("END")) {
+                for (int j = 0; j < 5; j++) {
+                    System.out.println("What is the temperature there?");
+                    Double temp = input.nextDouble();
+                    arrL.add(temp);
+
+
+                }
+                avgTemp();
+                a.put(city, avgTemp());
+                //System.out.println("Please list a city: (To Exit type END)");
+                //city = input.next();
+                arrL.clear();
             }
-            System.out.println("Please list a city: (To Exit type END)");
-            city = input.next();
+            else {
+                display(a);
+                System.exit(0);
+            }
         }
+        System.out.println(a);
 
+    }
 
+    static double avgTemp(){
+        double avg = 0;
+        double total = 0;
+        for (int i = 0; i < arrL.size(); i++) {
+
+            total = arrL.get(i) + total;
+
+        }
+        avg = total / arrL.size();
+        //System.out.println(avg);
+        return avg;
+    }
+    static void display(Map<String, Double> a){
         System.out.println(a);
 
 
